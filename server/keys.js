00a46ffc78eb42f681a60329d044e4f76803f10b
bhagath <bhagathyennemajalu@gmail.com>
2020-01-10 21:51:35 +0530

    server

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    redisHost: process.env.REDIS_HOST,
+    redisPort: process.env.REDIS_PORT,
+    pgUser: process.env.PG_USER,
+    pgHost: process.env.PG_HOST,
+    pgPassword: process.env.PG_PASSWORD,
+    pgPort: process.env.PG_PORT,
+    pgDatabase: process.env.PG_DATABASE
+}
